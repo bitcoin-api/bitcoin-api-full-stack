8b2c692c62bdb2d248c0097a172704f419efb029
Michael Stecky-Efantis <m.stecky.efantis@gmail.com>
2020-07-30 21:16:56 -0400

    Update getEmailHtml.js

--- 2-api/routes/exchange/users/POST/addNewUser/sendVerificationCodeEmail/getEmailHtml.js
+++ 2-api/routes/exchange/users/POST/addNewUser/sendVerificationCodeEmail/getEmailHtml.js
@@ -19,6 +19,8 @@ module.exports = Object.freeze( ({
     <style>
       /* -------------------------------------
           GLOBAL RESETS
+
+          email template source: https://github.com/leemunroe/responsive-html-email-template
       ------------------------------------- */
       
       /*All the styling goes here*/
