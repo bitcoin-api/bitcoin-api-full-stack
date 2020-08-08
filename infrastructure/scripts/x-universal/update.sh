97b16ebecb5f77516ac7a351e3aabdd1c089fd74
Michael Stecky-Efantis <m.stecky.efantis@gmail.com>
2020-08-07 23:47:26 -0400

    working on readme and infrastructure setup

--- infrastructure/scripts/x-universal/update.sh
+++ infrastructure/scripts/x-universal/update.sh
@@ -4,6 +4,7 @@
 pushd ../../../0-commonCode/general
 
 npm install orgasm@latest --save
+npm install
 
 popd
 
@@ -17,6 +18,7 @@ npm install drf@latest --save
 npm install drq@latest --save
 npm install get-redis-client@latest --save
 npm install orgasm@latest --save
+npm install
 
 popd
 
@@ -25,6 +27,87 @@ pushd ../../../0-commonCode/exchange
 
 npm install @bitcoin-api.io/common-private@latest --save
 npm install orgasm@latest --save
+npm install
 
 popd
 
+
+pushd ../../../1-backend/commonUtilities
+
+npm install @bitcoin-api.io/common-private@latest --save
+npm install bqe@latest --save
+npm install
+
+popd
+
+
+pushd ../../../1-backend/calzoneSun
+
+npm install @bitcoin-api.io/common-private@latest --save
+npm install orgasm@latest --save
+npm install
+
+popd
+
+
+pushd ../../../1-backend/feeFee
+
+npm install @bitcoin-api.io/common-private@latest --save
+npm install
+
+popd
+
+
+pushd ../../../1-backend/giraffeDeploy/commonUtilities
+
+npm install @bitcoin-api.io/common-private@latest --save
+npm install
+
+popd
+
+
+pushd ../../../1-backend/giraffeDeploy/giraffe
+
+npm install @bitcoin-api.io/common-private@latest --save
+npm install
+
+popd
+
+
+pushd ../../../1-backend/giraffeDeploy/tree
+
+npm install @bitcoin-api.io/common-private@latest --save
+npm install
+
+popd
+
+
+pushd ../../../1-backend/korg
+
+npm install @bitcoin-api.io/common-private@latest --save
+npm install @bitcoin-api.io/common-exchange@latest --save
+npm install
+
+popd
+
+
+pushd ../../../1-backend/theomega
+
+npm install @bitcoin-api.io/common-private@latest --save
+npm install @bitcoin-api.io/common-exchange@latest --save
+npm install orgasm@latest --save
+npm install
+
+popd
+
+
+pushd ../../../2-api
+
+npm install @bitcoin-api.io/common-bridge@latest --save
+npm install @bitcoin-api.io/common-general@latest --save
+npm install @bitcoin-api.io/common-private@latest --save
+npm install @bitcoin-api.io/common-exchange@latest --save
+npm install orgasm@latest --save
+npm install
+
+popd
